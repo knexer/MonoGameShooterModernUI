67f0c46cd769a9a215c79ffacbe6acbfe7f41cf4
Taylor Bergquist <knexer@gmail.com>
2012-11-01 17:26:52 -0500

    Implemented spawning entities at a parent entity's position
    This allows, among other things, bullets to be fired from the player's
    ship.

---
+++ Game1/Game1/Components/SpawnEntityAtPosition.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shooter.Components
+{
+    public class SpawnEntityAtPositionComponent : IComponent
+    {
+        public Entity toSpawn;
+
+        public IComponent Clone()
+        {
+            SpawnEntityAtPositionComponent spawner = new SpawnEntityAtPositionComponent();
+            spawner.toSpawn = toSpawn.Clone();
+
+            return spawner;
+        }
+    }
+}
