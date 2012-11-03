d235424ebc8e9ec714dd1b58e4025f87005bd7cb
Taylor Bergquist <knexer@gmail.com>
2012-11-03 02:11:23 -0500

    Began implementation of actually destroying marked entities

---
+++ Game1/Game1/Systems/RemoveEntitiesMarkedForDestructionSystem.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Xna.Framework;
+using Shooter.EntityManagers;
+using Shooter.Systems.MarkForDestructionSystems;
+using Shooter.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shooter.Systems
+{
+    public class RemoveEntitiesMarkedForDestructionSystem : ASystem
+    {
+        private ListEntityManager entities;
+
+        public RemoveEntitiesMarkedForDestructionSystem()
+        {
+            SetParents(new List<Type>() { typeof(PostEntityMarkForDestructionSystem) });
+            SetReqTypes(new List<Type>() { typeof(MarkedForDeathComponent) });
+        }
+
+        public override void Initialize(EntityManagerManager entityStorage)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Process(EntityManagerManager toProcess, GameTime gameTime)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
