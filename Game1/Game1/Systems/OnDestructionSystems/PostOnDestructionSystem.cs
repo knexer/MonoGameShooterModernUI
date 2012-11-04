819d5fe6e0f196b93c5e04e1e37b642fbea88603
Taylor Bergquist <knexer@gmail.com>
2012-11-03 20:13:56 -0500

    Added a new system phase for reacting to entity destruction
    Added two dummy systems for this phase.

---
+++ Game1/Game1/Systems/OnDestructionSystems/PostOnDestructionSystem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shooter.Systems.OnDestructionSystems
+{
+    public class PostOnDestructionSystem : ASystem
+    {
+        public PostOnDestructionSystem()
+        {
+            SetParents(new List<Type>() { typeof(PreOnDestructionSystem) });
+        }
+
+        public override void Initialize(EntityManagers.EntityManagerManager entityStorage)
+        {
+        }
+
+        public override void Process(EntityManagers.EntityManagerManager toProcess, Microsoft.Xna.Framework.GameTime gameTime)
+        {
+        }
+    }
+}
