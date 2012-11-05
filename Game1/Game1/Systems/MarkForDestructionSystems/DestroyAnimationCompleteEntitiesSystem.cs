e0e735116a51f2162ffed90533191d3211c4cfc2
Taylor Bergquist <knexer@gmail.com>
2012-11-05 03:45:04 -0600

    Implemented entity destruction on animation completion

---
+++ Game1/Game1/Systems/MarkForDestructionSystems/DestroyAnimationCompleteEntitiesSystem.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Xna.Framework;
+using Shooter.EntityManagers;
+using Shooter.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shooter.Systems.MarkForDestructionSystems
+{
+    public class DestroyAnimationCompleteEntitiesSystem : ASystem
+    {
+        private ListEntityManager entities;
+
+        public DestroyAnimationCompleteEntitiesSystem()
+        {
+            SetReqTypes(new List<Type>() { typeof(DestroyedWhenAnimationCompleteComponent), typeof(AnimationComponent) });
+            SetParents(new List<Type>() { typeof(PreMarkForDestructionSystem) });
+            SetChildren(new List<Type>() { typeof(PostMarkForDestructionSystem) });
+        }
+
+        public override void Initialize(EntityManagerManager entityStorage)
+        {
+            entities = entityStorage.DefaultManager;
+        }
+
+        public override void Process(EntityManagerManager toProcess, GameTime gameTime)
+        {
+            foreach (Entity ent in entities.entities)
+            {
+                if (IsApplicableTo(ent))
+                {
+                    AnimationComponent anim = (AnimationComponent)ent.components[typeof(AnimationComponent)];
+
+                    //If the animation is complete
+                    if (anim.CurrentFrameIndex == -1)
+                    {
+                        ent.AddComponent(new MarkedForDeathComponent());
+                    }
+                }
+            }
+        }
+    }
+}
