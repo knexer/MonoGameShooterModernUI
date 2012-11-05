9ea1a49c9942f44c8739b97074288c893c974cd2
Taylor Bergquist <knexer@gmail.com>
2012-11-05 03:27:45 -0600

    Added a component/system combo to support spawning explosions on death
    It's more flexible than that - arbitrary components are added in
    reaction to the death

---
+++ Game1/Game1/Components/AddComponentOnDestructionComponent.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shooter.Components
+{
+    public class AddComponentOnDestructionComponent : IComponent
+    {
+        public IComponent ToAdd;
+
+        public IComponent Clone()
+        {
+            AddComponentOnDestructionComponent ret = new AddComponentOnDestructionComponent();
+            ret.ToAdd = ToAdd;
+
+            return ret;
+        }
+    }
+}
---
+++ Game1/Game1/Systems/OnDestructionSystems/AddComponentOnDestructionSystem.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Xna.Framework;
+using Shooter.Components;
+using Shooter.EntityManagers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shooter.Systems.OnDestructionSystems
+{
+    public class AddComponentOnDestructionSystem : ASystem
+    {
+        private ListEntityManager entities;
+
+        public AddComponentOnDestructionSystem()
+        {
+            SetReqTypes(new List<Type>() { typeof(AddComponentOnDestructionComponent), typeof(MarkedForDeathComponent) });
+            SetParents(new List<Type>() { typeof(PreOnDestructionSystem) });
+            SetChildren(new List<Type>() { typeof(PostOnDestructionSystem) });
+        }
+
+        public override void Initialize(EntityManagerManager entityStorage)
+        {
+            entities = entityStorage.DefaultManager;
+        }
+
+        public override void Process(EntityManagerManager toProcess, GameTime gameTime)
+        {
+            foreach (Entity ent in entities.entities)
+            {
+                if (IsApplicableTo(ent))
+                {
+                    AddComponentOnDestructionComponent wrapper = (AddComponentOnDestructionComponent)ent.components[typeof(AddComponentOnDestructionComponent)];
+                    IComponent compTemplate = wrapper.ToAdd;
+
+                    IComponent comp = compTemplate.Clone();
+
+                    ent.AddComponent(comp);
+                }
+            }
+        }
+    }
+}
